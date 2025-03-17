14aca9be2b736e267920c749854a7b2fec6615ce
danxguerrero <danxguerrero@yahoo.com>
2025-03-16 23:34:30 -0400

    Get the first 100 timestamps from HackerNews.

--- index.js
+++ index.js
@@ -9,6 +9,32 @@ async function sortHackerNewsArticles() {
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
+
+  // Get all of the timestamps for the first 100 articles
+  let timestamps = [];
+
+  const fetchNewTimestamps = async () => {
+    const locator = page.locator('.age');
+    const newTimestamps = await locator.evaluateAll(articles => articles.map(article => article.getAttribute('title')));
+    if (timestamps.length < 90) {
+      timestamps = timestamps.concat(newTimestamps);
+    } else {
+      timestamps = timestamps.concat(newTimestamps.slice(0, 100 - timestamps.length))
+    };
+  };
+
+  await fetchNewTimestamps();
+
+  while (timestamps.length < 100) {
+    const moreButton = page.locator('a.morelink');
+    await moreButton.click();
+    await page.waitForTimeout(1000);
+    await fetchNewTimestamps();
+  };
+
+  console.log(timestamps.length)
+
+  await browser.close();
 }
 
 (async () => {
